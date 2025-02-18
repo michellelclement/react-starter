106825649ec2812b9e3d252e6729849da5c63c16
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2025-02-18 09:38:12 +0000

    Intro ro props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-// Import component
+// Import components
 import TestComponent from "./TestComponent";
+import Tweet from "./components/Tweet";
+import CreateTweet from "./components/CreateTweet";
 
 function App() {
+  const name = "Michelle";
+  const message = "This is the tweet";
+
   return (
     <div>
       <h1>Hello React</h1>
-      <TestComponent />
+      {/* <TestComponent /> */}
+      <CreateTweet />
+      <Tweet name={name} message={message} />
     </div>
   );
 }
---
+++ src/components/CreateTweet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateTweet = () => {
+  return (
+    <form>
+      <textarea cols="40" rows="10" name="" id=""></textarea>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default CreateTweet;
